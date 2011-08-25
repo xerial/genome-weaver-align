47bbed4ff525b9bbedb20a58de57823086f7230b
Taro L. Saito <leo@xerial.org>
2011-08-25 15:56:52 +0900

    Add smith waterman result holder

---
+++ src/main/java/org/utgenome/weaver/align/record/SWResult.java
@@ -0,0 +1,57 @@
+/*--------------------------------------------------------------------------
+ *  Copyright 2011 utgenome.org
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *--------------------------------------------------------------------------*/
+//--------------------------------------
+// genome-weaver Project
+//
+// SWResult.java
+// Since: 2011/08/22
+//
+// $URL$ 
+// $Author$
+//--------------------------------------
+package org.utgenome.weaver.align.record;
+
+import org.xerial.lens.JSONLens;
+
+/**
+ * smith-waterman alignment result
+ * 
+ * @author leo
+ * 
+ */
+public class SWResult
+{
+    public final int      tailPos;
+    public final int      diff;
+    public final SWResult chain;
+
+    public SWResult(int tailPos, int diff) {
+        this.tailPos = tailPos;
+        this.diff = diff;
+        this.chain = null;
+    }
+
+    public SWResult(SWResult head, SWResult tail) {
+        this.tailPos = head.tailPos;
+        this.diff = head.diff;
+        this.chain = tail;
+    }
+
+    @Override
+    public String toString() {
+        return JSONLens.toJSON(this);
+    }
+}
