88ccc636d937b354c13ac525f501c83bf18bb316
Taro L. Saito <leo@xerial.org>
2012-06-03 11:25:24 +0900

    working on ACGTSequence

---
+++ gw-lens/src/test/scala/xerial/silk/glens/ACGTSequenceTest.scala
@@ -0,0 +1,15 @@
+//--------------------------------------
+//
+// ACGTSequenceTest.scala
+// Since: 2012/06/03 10:50 AM
+//
+//--------------------------------------
+
+package xerial.silk.glens
+
+/**
+ * @author leo
+ */
+class ACGTSequenceTest extends SilkSpec {
+
+}
