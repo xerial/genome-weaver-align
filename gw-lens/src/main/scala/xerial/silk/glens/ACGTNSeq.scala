8e2586c1b1c1c6ecf92fb835d06f7070e376bd6c
Taro L. Saito <leo@xerial.org>
2012-06-19 18:48:55 +0900

    working on hash function

--- gw-lens/src/main/scala/xerial/silk/glens/ACGTNSeq.scala
+++ gw-lens/src/main/scala/xerial/silk/glens/ACGTNSeq.scala
@@ -31,13 +31,12 @@ trait DNA3bitEncoding {
 
   def blockOffset(basePos: Long): Long = (basePos & 0x3FL) // This value must be Long to enable 64-bit shift using this value
 
-  def numFilledBlocks(numBases:Long) = (numBases / 64L * 3L).toInt
+  def numFilledBlocks(numBases: Long) = (numBases / 64L * 3L).toInt
 
 }
 
 
 object ACGTNSeq {
-
 
 
 }
@@ -47,82 +46,83 @@ object ACGTNSeq {
  *
  * @author leo
  */
-class ACGTNSeq(private val seq:Array[Long], val numBases:Long)
+class ACGTNSeq(private val seq: Array[Long], val numBases: Long)
   extends DNASeq[ACGTNSeq]
   with DNA3bitEncoding {
 
-  lazy private val hash : Int = {
+  lazy private val hash: Int = {
     var h = numBases * 31L
     var pos = 0
     val filledBlockSize = numFilledBlocks(numBases)
-    while(pos < filledBlockSize) {
+    while (pos < filledBlockSize) {
       h += seq(pos) * 31L
       pos += 1
     }
     val offset = (numBases % 64L).toInt
     if (offset > 0) {
       h += (seq(pos) & (~0L << 64 - offset)) * 31L
-      h += (seq(pos + 1) & (if(offset < 32) ~0L << (32 - offset) * 2 else ~0L)) * 31L
-      h += (seq(pos + 2) & (if(offset < 32) 0L else ~0L << (64 - offset) * 2)) * 31L
+      h += (seq(pos + 1) & (if (offset < 32) ~0L << (32 - offset) * 2 else ~0L)) * 31L
+      h += (seq(pos + 2) & (if (offset < 32) 0L else ~0L << (64 - offset) * 2)) * 31L
     }
     h.toInt
   }
 
   override def hashCode = hash
 
-  override def equals(obj:Any) = {
-    if (!(obj.isInstanceOf[ACGTNSeq]))
-      return false
+  override def equals(obj: Any) = {
 
-    val other: ACGTNSeq = obj.asInstanceOf[ACGTNSeq]
-
-    if (this.numBases != other.numBases)
-      return false
-
-    val filledBlockSize: Int = numFilledBlocks(numBases)
-    var pos: Int = 0
-
-    
-
-
-    while (pos < filledBlockSize) {
-      if (this.seq(pos) != other.seq(pos)) return false
-      pos += 1;
-      pos - 1
-    }
-
-
-    val offset: Int = (numBases % 64L).asInstanceOf[Int]
-
-
-    if (offset > 0) {
-      val mask: Long = new Array[Long](3)
-      mask(0) = ~0L << 64 - offset
-      mask(1) = if (offset < 32) ~0L << (32 - offset) * 2 else ~0L
-      mask(2) = if (offset <= 32) 0L else ~0L << (64 - offset) * 2
-      {
-        var i: Int = 0
-        while (i < mask.length) {
-          {
-            if ((seq(pos + i) & mask(i)) != (other.seq(pos + i) & mask(i))) return false
-          }
-          ({
-            i += 1; i - 1
-          })
-        }
+    def eligible : Option[ACGTNSeq] = {
+      if (!(obj.isInstanceOf[ACGTNSeq]))
+        None
+      else {
+        val other: ACGTNSeq = obj.asInstanceOf[ACGTNSeq]
+        if (this.numBases != other.numBases)
+          None
+        else
+          Some(other)
       }
     }
+
+    val isEqual = eligible.flatMap {
+      other =>
+
+        val filledBlockSize: Int = numFilledBlocks(numBases)
+        var pos: Int = 0
+        while (pos < filledBlockSize) {
+          if (this.seq(pos) != other.seq(pos))
+            return false
+          pos += 1
+        }
+
+        val offset: Int = (numBases % 64L).toInt
+
+        if (offset > 0) {
+          val mask: Array[Long] = new Array[Long](3)
+          mask(0) = ~0L << 64 - offset
+          mask(1) = if (offset < 32) ~0L << (32 - offset) * 2 else ~0L
+          mask(2) = if (offset <= 32) 0L else ~0L << (64 - offset) * 2
+          var i: Int = 0
+          while (i < mask.length) {
+            if ((seq(pos + i) & mask(i)) != (other.seq(pos + i) & mask(i)))
+              return false
+            i += 1
+          }
+        }
+        true
+    }
+
+    isEqual.
   }
 
 
   def apply(index: Long) = {
-    val pos  : Int = blockIndex(index)
-    val offset : Int = blockOffset(index).toInt
+    val pos: Int = blockIndex(index)
+    val offset: Int = blockOffset(index).toInt
     val shift = 62L - ((index & 0x1FL) << 1)
 
-    val nFlag : Long  = seq(pos * 3) & (1L << (63 - offset))
-    val code : Int = (seq(pos * 3 + (offset >> 5) + 1) >>> shift).toInt & 0x03
-    if(nFlag == 0) DNA(code) else DNA.N
+    val nFlag: Long = seq(pos * 3) & (1L << (63 - offset))
+    val code: Int = (seq(pos * 3 + (offset >> 5) + 1) >>> shift).toInt & 0x03
+    if (nFlag == 0) DNA(code) else DNA.N
   }
 
   def slice(start: Long, end: Long) = null
